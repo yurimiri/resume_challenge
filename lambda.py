08572ae205f94bddaf34706d119074479a3ec775
Glen <glen910115@gmail.com>
2024-05-29 17:09:27 +0900

    update all

---
+++ lambda.py
@@ -0,0 +1,55 @@
+import json
+import boto3
+from boto3.dynamodb.conditions import Key
+
+dynamodb = boto3.resource("dynamodb")
+table_name = "YourDynamoDBTableName"
+table = dynamodb.Table(table_name)
+
+
+def lambda_handler(event, context):
+    print("##########")
+    path = event["requestContext"]["http"]["path"]
+    print(path)
+    method = event["requestContext"]["http"]["method"]
+    print(method)
+    print("##########")
+    if path == "/visit" and method == "GET":
+        print("방문!")
+        return handle_visit()
+    elif path == "/likes" and method == "GET":
+        print("좋아요 조회!")
+        return handle_likes()
+    elif path == "/like" and method == "POST":
+        print("좋아요 추가!")
+        return handle_like()
+    else:
+        return {"statusCode": 400, "body": json.dumps({"message": "Bad Request"})}
+
+
+def handle_visit():
+    response = table.update_item(
+        Key={"id": "visit_count"},
+        UpdateExpression="ADD visits :inc",
+        ExpressionAttributeValues={":inc": 1},
+        ReturnValues="UPDATED_NEW",
+    )
+    visits = response["Attributes"]["visits"]
+    return {"statusCode": 200, "body": json.dumps({"visits": visits})}
+
+
+def handle_likes():
+    response = table.get_item(Key={"id": "like_count"})
+    likes = response.get("Item", {}).get("likes", 0)
+    return {"statusCode": 200, "body": json.dumps({"likes": likes})}
+
+
+def handle_like():
+    response = table.update_item(
+        Key={"id": "like_count"},
+        UpdateExpression="ADD likes :inc",
+        ExpressionAttributeValues={":inc": 1},
+        ReturnValues="UPDATED_NEW",
+    )
+    likes = response["Attributes"]["likes"]
+    return {"statusCode": 200, "body": json.dumps({"likes": likes})}
---
+++ s3.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# 변수 설정
+BUCKET_NAME=YOUR-BUCKET-NAME
+REGION="ap-northeast-2"
+INDEX_FILE="index.html"
+
+# S3 버킷 생성
+echo "S3 bucket 생성 중...\n\n"
+aws s3api create-bucket --bucket $BUCKET_NAME --region $REGION --create-bucket-configuration LocationConstraint=$REGION
+
+# 정적 웹사이트 호스팅 활성화
+echo "정적 웹사이트 호스팅 활성화 중...\n\n"
+aws s3 website s3://$BUCKET_NAME/ --index-document $INDEX_FILE
+
+# 퍼블릭 액세스 차단 해제
+echo "퍼블릭 액세스 차단 해제 중...\n\n"
+aws s3api delete-public-access-block --bucket $BUCKET_NAME
+
+# 버킷 정책 설정
+echo "버킷 정책 설정 중...\n\n"
+echo '{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::'$BUCKET_NAME'/*"
+        }
+    ]
+}' > bucket-policy.json
+aws s3api put-bucket-policy --bucket $BUCKET_NAME --policy file://bucket-policy.json
+rm bucket-policy.json
+
+# index.html 파일 업로드
+echo "index.html 파일 업로드 중...\n\n"
+aws s3 cp $INDEX_FILE s3://$BUCKET_NAME/
+
+# 웹사이트 URL 출력
+echo "배포된 웹사이트 주소 :"
+echo "http://$BUCKET_NAME.s3-website.$REGION.amazonaws.com"
+
+# 삭제 명령어
+# echo "To delete the bucket and all its contents, run the following commands:"
+# echo "aws s3 rm s3://$BUCKET_NAME --recursive"
+# echo "aws s3api delete-bucket --bucket $BUCKET_NAME --region $REGION"
